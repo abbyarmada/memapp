a0c8b1c1ada6ab95bb7300de02db22a19381c7f7
petercunning <peter.cunning@gmail.com>
2014-10-09 15:35:12 +0000

    Travis build failed

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -16,12 +16,3 @@ module ControllerHelpers
      #end
   end
 end
-
-module ValidUserRequestHelper
-
-  # for use in request specs
-  def sign_in_as_a_valid_user
-    @user ||= FactoryGirl.create :user
-    post_via_redirect user_session_path, 'user[email]' => @user.email, 'user[password]' => @user.password
-  end
-end

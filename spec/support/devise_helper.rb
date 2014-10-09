6ce4f75c3203f4e827c5434bb281ab67aae1c600
petercunning <peter.cunning@gmail.com>
2014-10-09 21:24:56 +0100

    Update devise_helper.rb

--- spec/support/devise_helper.rb
+++ spec/support/devise_helper.rb
@@ -21,7 +21,14 @@ end
 
 
 
+module ValidUserRequestHelper
 
+  # for use in request specs
+  def sign_in_as_a_valid_user
+    @user ||= FactoryGirl.create :user
+    post_via_redirect user_session_path, 'user[email]' => @user.email, 'user[password]' => @user.password
+  end
+end
 
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller

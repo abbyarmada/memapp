a79f903ceb5c3f2b80094344677b11db3e8e62e5
petercunning <peter.cunning@gmail.com>
2014-11-27 13:51:32 +0000

    Update to fix travis

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -2,5 +2,6 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
   config.include ValidUserRequestHelper, type: :request
+  config.include DeviseHelper, type: :request
   config.include Warden::Test::Helpers
 end

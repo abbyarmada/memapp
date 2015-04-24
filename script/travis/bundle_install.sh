8324024232304436790e7af7418e335ef978158b
petercunning <peter.cunning@gmail.com>
2015-04-24 14:23:33 +0100

    Update bundle_install.sh

--- script/travis/bundle_install.sh
+++ script/travis/bundle_install.sh
@@ -2,8 +2,8 @@
 # script/travis/bundle_install.sh
  
 ARCHITECTURE=`uname -m`
-FILE_NAME="$BUNDLE_ARCHIVE-$ARCHITECTURE.tgz"
- 
+FILE_NAME="$TRAVIS_BRANCH-$ARCHITECTURE.tgz"
+
 cd ~
 wget -O "remote_$FILE_NAME" "ftp://$CI_FTP_URL/$FILE_NAME" && tar -xf "remote_$FILE_NAME" 
 wget -O "remote_$FILE_NAME.sha2" "ftp://$CI_FTP_URL/$FILE_NAME.sha2"

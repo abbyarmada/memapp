d4b6ba3658171801be2cb1225fea43c3b012b7aa
Peter Cunning <peter.cunning@gmail.com>
2015-12-22 16:48:10 +0000

    nil date conversion [ci skip]

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -76,7 +76,7 @@ class MembersController < ApplicationController
             p.email_address,
             p.mobile_phone,
             p.member.privilege.name,
-            p.member.renew_date.to_date,
+            p.member.renew_date.to_date rescue nil,
             p.member.id.to_s + Time.now.year.to_s.slice(2,2) + i.to_s,
             p.salutation,
             p.member.name_no,

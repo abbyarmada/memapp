f5e2ddc8145b168d4d9da5ce10907c26490f2760
Peter Cunning <peter.cunning@storyful.com>
2016-07-19 21:08:57 +0100

    Update payment helper

--- app/helpers/payment_helper.rb
+++ app/helpers/payment_helper.rb
@@ -1,5 +1,6 @@
 module PaymentHelper
-  def g_chart_mems(endmonth, endday)
+  now = Time.now.utc
+  def g_chart_mems(endmonth = now.month.to_s, endday = now.end_of_month.day.to_s)
     now = Time.now.utc
     years = 5
     types = []
